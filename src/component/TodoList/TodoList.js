96ad178d4b08069fda2083fc13e3ef51b7f58bad
junseo <qkrwnstj0413@naver.com>
2020-05-27 17:48:55 +0900

    Create todo-list

---
+++ src/component/TodoList/TodoList.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import TodoItem from '../TodoItem';
+class TodoList extends Component {
+    render() {
+        const {todos, onToggle, onRemove} = this.props;
+        const TodoList =todos.map(
+            todo=>(
+                <TodoItem
+                key={todo.id}
+                done={todo.done}
+                onToggle={()=>onToggle(todo.id)}
+                onRemove={()=>onRemove(todo.id)}>
+                    {todo.text}
+                </TodoItem>
+            )
+        )
+        return (
+            <div>
+                {TodoList}
+            </div>
+        );
+    }
+}
+
+export default TodoList;
